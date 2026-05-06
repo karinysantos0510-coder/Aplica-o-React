8cf9573847e856b2eda088e2df98007ee77586e6
karinysantos0510-coder <karinysantos0510@gmail.com>
2026-05-06 02:58:15 -0300

    Force deploy: removendo build e atualizando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      {/* O componente Rotas decide qual página mostrar */}
       <Rotas /> 
     </BrowserRouter>
+    // Build final verificado localmente
   );
 }
 
